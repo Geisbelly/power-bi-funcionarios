4171d8a44c68ea9c4916b980d4f6c80eb8aabc75
Geisbelly <geisbelly19@gmail.com>
2024-10-01 02:41:22 -0300

    FuncionariosSalarios

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,101 +1,88 @@
-import Image from "next/image";
+"use client";
+import { FuncionraiosComuns, Gerente, Diretor } from '../../models/Funcionrios';
 
-export default function Home() {
+
+
+const Home: React.FC = () =>{
+
+  const salvarFuncionario = () =>{
+
+    const nome = document.getElementById('nome') as HTMLInputElement;
+    const salario = document.getElementById('salario') as HTMLInputElement;
+    const tempo = document.getElementById('tempo_empresa') as HTMLInputElement;
+    const cargo = document.getElementById('cargos') as HTMLSelectElement;
+
+    if (cargo.value === 'diretor'){
+      const func = new Diretor(nome.value,parseInt(tempo.value),parseInt(salario.value))
+      criarElementosHTML(func);
+    } else if (cargo.value === 'gerente'){
+      const func = new Gerente(nome.value,parseInt(tempo.value),parseInt(salario.value))
+      criarElementosHTML(func);
+    } else{
+      const func = new FuncionraiosComuns(nome.value,parseInt(tempo.value),parseInt(salario.value))
+      criarElementosHTML(func);
+    }
+    
+  }
+
+  const criarElementosHTML = (f: FuncionraiosComuns) => {
+    const divFun = document.createElement('li');
+    const nome = document.createElement('h3');
+    const tempo = document.createElement('p');
+    const cargo = document.createElement('p');
+    const salario = document.createElement('p');
+    const bonus = document.createElement('p');
+
+    nome.textContent =  f.getNome();
+    tempo.textContent = "Tempo de Empresa: "+f.getTempo_empresa()+" ano";
+    salario.textContent = "Salário: R$"+f.getSalario();
+    bonus.textContent = "Bônus: R$"+f.calcularBonus();
+
+    if( f instanceof Gerente){
+      cargo.textContent = "Cargo: Gerente";
+    } else if (f instanceof Diretor){
+      cargo.textContent = "Cargo: Diretor";
+    }else {
+      cargo.textContent = "Cargo: Funcionario Comum";
+    }
+
+    divFun.appendChild(nome);
+    divFun.appendChild(tempo);
+    divFun.appendChild(cargo);
+    divFun.appendChild(salario);
+    divFun.appendChild(bonus);
+
+    const prin = document.getElementById('funcionarios');
+    prin?.appendChild(divFun);
+
+  }
+
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+    <div id='main'>
+      <div id='formulario-container'>
+        <h1>Funcionarios</h1>
+        <div id='formularios'>
+          <div id='um'>
+            <input type="text" id='nome' placeholder='Nome do Funcionario' />
+            <select name="cargos" id="cargos">
+              <option value="comum">Funcionario Comum</option>
+              <option value="gerente">Gerente</option>
+              <option value="diretor">Diretor</option>
+            </select>
+          </div>
+          <div id='dois'>
+            <input type="text" id='salario' placeholder='Salário' />
+            <input type="number" id='tempo_empresa' placeholder='Tempo de empresa em anos' />
+          </div>
+          <button onClick={salvarFuncionario}>Salvar</button>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+      </div>
+      <div id='funcionarios-container'>
+        <ul id='funcionarios'></ul>
+      </div>
     </div>
   );
-}
+};
+
+
+export default Home;
